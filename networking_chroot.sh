2a7e0307e34d37aca45ecf58ba6d0fa2ff5efb4a
mike <mike@lcd-outlet.com>
2018-02-03 04:59:36 +0000

    networking

--- networking_chroot.sh
+++ networking_chroot.sh
@@ -1,15 +1,7 @@
 #https://grinux.wordpress.com/2012/02/24/setting-up-a-network-inside-a-chroot/
 
-The requirements are:
-Your chroot will be running under a main OS, which is already connected to the internet.
-You already have mounted the disk to chroot to.
+mount -o bind /sys ./sys
+mount -o bind /proc ./proc
+chroot . 
 
-# mount -o bind /sys /media/chrooted_disk/sys
-# mount -o bind /proc /media/chrooted_disk/proc
-# chroot /media/chrooted_disk
-
-You need to reset the network in order for the route/dns stuff to be setup.
-
-# dhclient eth0
-or
-# dhcpcd 
+dhclient eth0
